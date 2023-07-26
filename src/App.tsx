09149f0d7342b40269e79562dce3c04d1aeb082a
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-07-26 19:37:19 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import NavBar from '@/scenes/navbar';
 import { useState } from 'react';
+import { SelectedPage } from './shared/types';
+SelectedPage
+
+
 
 
 function App() {
-const [ selectedPage, setSelectedPage] = useState<string>("home");
+const [ selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return <div className="app bg-gray-20">
     <NavBar   
