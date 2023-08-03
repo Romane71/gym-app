7b8b037e78f1b18df109682c816e8a158089cd2b
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-08-03 05:55:19 +0100

    contact us page-1

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ SelectedPage;
 import Home from '@/scenes/home';
 import Benefits from '@/scenes/benefits';
 import OurClasses from '@/scenes/ourClasses';
+import ContactUs from './scenes/contactUs';
+
 
 
 
@@ -34,6 +36,7 @@ return () => window.removeEventListener("scroll", handleScroll)
     <Home setSelectedPage={setSelectedPage} />
     <Benefits setSelectedPage={setSelectedPage} />
     <OurClasses setSelectedPage={setSelectedPage} />
+    <ContactUs setSelectedPage={setSelectedPage} />
   </div>
 }
 
