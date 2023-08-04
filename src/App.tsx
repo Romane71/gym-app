853c5042b59abe3dcf7669c47943c8f5ed99f657
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-08-04 01:39:28 +0100

    Full footer section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from '@/scenes/home';
 import Benefits from '@/scenes/benefits';
 import OurClasses from '@/scenes/ourClasses';
 import ContactUs from './scenes/contactUs';
+import Footer from '@/scenes/footer'
 
 
 
@@ -37,6 +38,7 @@ return () => window.removeEventListener("scroll", handleScroll)
     <Benefits setSelectedPage={setSelectedPage} />
     <OurClasses setSelectedPage={setSelectedPage} />
     <ContactUs setSelectedPage={setSelectedPage} />
+    <Footer  />
   </div>
 }
 
